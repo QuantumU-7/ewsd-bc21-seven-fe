554eb1b30057d421cdf8e76cb5078ddad34b9a1f
T-N9 <thethoriumtension@gmail.com>
2025-02-23 08:28:45 +0700

    ✨ Latest Idea Home Page
    UI creation done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import LatestIdeaSection from '@/components/latest-idea-section'
 import PopularSection from '@/components/popular-section/PopularSection'
 import React from 'react'
 
@@ -5,6 +6,11 @@ const HomePage = () => {
   return (
     <div>
         <PopularSection/>
+        <LatestIdeaSection/>
+
+        <div className='h-screen'>
+
+        </div>
     </div>
   )
 }
---
+++ src/components/shared/common/idea-card/IdeaCard.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import React from "react";
+
+const IdeaCard = ({ id, title, description, image }) => {
+  return (
+    <div className="cursor-pointer">
+      <div className="flex flex-col lg:flex-row rounded-md overflow-hidden bg-white shadow-lg">
+        <div className="flex-1 p-8 flex flex-col justify-between gap-6">
+          <div className="space-y-4">
+            <p className="text-xl mt-4">{id}</p>
+            <h1 className="text-3xl text-primary">{title}</h1>
+          </div>
+
+          <p className="text-gray-500">{description}</p>
+        </div>
+
+        <div className="flex-1">
+          <Image className="w-full" src={image} width={300} height={200}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdeaCard;
