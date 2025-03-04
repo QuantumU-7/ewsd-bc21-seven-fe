0fffc6fb58e8cf01b5ec029dd28771311ce22b2c
T-N9 <thethoriumtension@gmail.com>
2025-03-04 15:36:14 +0700

    🐛 Create Idea page ssr fixed

---
+++ src/app/create/page.js
@@ -0,0 +1,15 @@
+'use client'
+import dynamic from "next/dynamic";
+
+const CreateIdeaForm = dynamic(() => import("@/components/create-idea-form"), {
+  ssr: false, 
+});
+const CreateIdeaPage = () => {
+  return (
+    <div>
+      <CreateIdeaForm/>
+    </div>
+  )
+}
+
+export default CreateIdeaPage
