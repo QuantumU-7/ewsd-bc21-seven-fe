a446cc86fc9b02f2a7bd17079c3f96a059b4a475
MayK <ms.maykhattar@gmail.com>
2025-03-10 22:45:52 +0700

    🎨 Fix layout for staff flow
    fix layout for staff flow

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -3,8 +3,8 @@ import { AdminSidebar } from "@/components/admin-sidebar/AdminSidebar";
 export default function CategoryLayout({ children }) {
   return (
     <SidebarProvider>
-          <AdminSidebar />
-          <div className="w-full ">{children}</div>
+      <AdminSidebar />
+      <div className="w-full ">{children}</div>
     </SidebarProvider>
   );
 }
