b7651958a2b2251eb2d3c7ddb7ebb3de13a0b468
phyowaiaung <phyo@xenoptics.com>
2025-04-22 23:44:56 +0700

    most active users

--- src/components/admin-reports/components/MostActiveUsersTable.js
+++ src/components/admin-reports/components/MostActiveUsersTable.js
@@ -1,38 +1,8 @@
-import { useRouter } from "next/navigation";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
-import { Ellipsis } from "lucide-react";
-import { ConfirmationBox } from "@/components/shared/common/Dialog/ConfirmationBox";
 import { TableCell, TableRow } from "@/components/ui/table";
 import CommonTable from "@/components/shared/common/Table";
 import CommonPagination from "@/components/shared/common/Pagination";
-import { useState } from "react";
-import { deleteUserById } from "@/services/userManagementService";
-import { toast } from "sonner";
-
 const MostActiveUsersTable = ({ users, loading, pagination, handlePageChange }) => {
-  const [openConfirmBox, setOpenConfirmBox] = useState(false);
-  const [deleteId, setDeleteId] = useState(null);
-  const router = useRouter();
 
-  // const handleEdit = (userId) => {
-  //   router.push(`/admin/users/${userId}`);
-  // };
-
-  // const handleDelete = async (id) => {
-  //   try {
-  //      await deleteUserById(id);
-  //      toast.success("User deleted successfully");
-  //     // After successful deletion, refresh the current page
-  //     handlePageChange(pagination.currentPage);
-  //   } catch (error) {
-  //     toast.error("Error deleting user:", error);
-  //   }
-  // };
- 
   const tableBody = (
     <>
       {users.map((user) => (
@@ -41,35 +11,7 @@ const MostActiveUsersTable = ({ users, loading, pagination, handlePageChange }) 
           <TableCell>{user.user.role.name}</TableCell>
           <TableCell>{user.user.email}</TableCell>
           <TableCell>{user.user.department.name}</TableCell>
-          <TableCell>{user.user.role.name}</TableCell>
-          {/* <TableCell>
-            <Popover>
-              <PopoverTrigger asChild>
-                <div className="cursor-pointer flex justify-end">
-                  <Ellipsis className="text-gray-500" />
-                </div>
-              </PopoverTrigger>
-              <PopoverContent className="w-[110px] rounded-xl p-0 flex flex-col text-center">
-                <div className="border-t"></div>
-                <p
-                  className="text-sm p-2 cursor-pointer hover:bg-gray-100"
-                  onClick={() => handleEdit(user.user.id)}
-                >
-                  Edit
-                </p>
-                <div className="border-t"></div>
-                <p
-                  className="text-sm p-2 cursor-pointer hover:bg-gray-100"
-                  onClick={() => {
-                    setDeleteId(user.user.id);
-                    setOpenConfirmBox(true);
-                  }}
-                >
-                  Delete
-                </p>
-              </PopoverContent>
-            </Popover>
-          </TableCell> */}
+          {/* <TableCell>{user.user.role.name}</TableCell> */}
         </TableRow>
       ))}
     </>
@@ -78,7 +20,7 @@ const MostActiveUsersTable = ({ users, loading, pagination, handlePageChange }) 
   return (
     <>
       <CommonTable
-        columns={["Name", "User Role", "Email", "Department", "Role"]}
+        columns={["Name", "User Role", "Email", "Department"]}
         loading={loading}
         tableBody={tableBody}
       />
@@ -93,20 +35,6 @@ const MostActiveUsersTable = ({ users, loading, pagination, handlePageChange }) 
           />
         </div>
       )}
-      {/* <ConfirmationBox
-        title="Delete User"
-        description="Are you sure you want to delete this user?"
-        onConfirm={() => {
-          if (deleteId) {
-            handleDelete(deleteId);
-            setDeleteId(null);
-            setOpenConfirmBox(false);
-          }
-        }}
-        onCancel={() => setOpenConfirmBox(false)}
-        isOpen={openConfirmBox}
-        setIsOpen={setOpenConfirmBox}
-      /> */}
     </>
   );
 };
